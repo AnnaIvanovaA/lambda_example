500b7e330a7bd372d27977dd30f2e7401de69a0a
Anna.Ivanova <anna.ivanova@jetbrains.com>
2018-03-20 14:18:48 +0300

    class Test added

--- src/com/jet/Test.java
+++ src/com/jet/Test.java
@@ -1,4 +1,17 @@
 package com.jet;
 
+import java.util.List;
+
 public class Test {
+
+    public static void main(String[] args) {
+        List<Person> pl = Person.createShortList();
+
+        System.out.println("all persons list");
+
+        pl.forEach( p -> p.printWesternName() );
+
+        System.out.println("done");
+    }
+
 }
